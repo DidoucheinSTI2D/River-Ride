33cace2bb1f0607143cddcdd6e84738bb4f6cfa1
Lucass307 <115079040+Lucass307@users.noreply.github.com>
2023-05-30 10:35:56 +0200

    Add files via upload

--- MasterTheWeb.sql
+++ MasterTheWeb.sql
@@ -5,6 +5,9 @@ SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
 -- -----------------------------------------------------
+-- Schema mydb
+-- -----------------------------------------------------
+-- -----------------------------------------------------
 -- Schema MasterTheWeb
 -- -----------------------------------------------------
 
@@ -15,192 +18,209 @@ CREATE SCHEMA IF NOT EXISTS `MasterTheWeb` DEFAULT CHARACTER SET utf8 ;
 USE `MasterTheWeb` ;
 
 -- -----------------------------------------------------
--- Table `MasterTheWeb`.`Utilisateur`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `MasterTheWeb`.`Utilisateur` (
-  `id_Utilisateur` INT NOT NULL AUTO_INCREMENT,
-  `Pseudo` VARCHAR(30) NOT NULL,
-  `Admin` INT NOT NULL,
-  `e-mail` VARCHAR(45) NOT NULL,
-  `Mot_de_passe` VARCHAR(45) NOT NULL,
-  `date_de_naissance` DATE NOT NULL,
-  PRIMARY KEY (`id_Utilisateur`))
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
 -- Table `MasterTheWeb`.`Chat`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `MasterTheWeb`.`Chat` (
-  `id_Chat` INT NOT NULL AUTO_INCREMENT,
-  `Id_rédacteur` INT NOT NULL,
+  `id_Chat` INT(11) NOT NULL AUTO_INCREMENT,
+  `privé/publique` VARCHAR(8) NOT NULL,
+  `Id_rédacteur` INT(11) NOT NULL,
   PRIMARY KEY (`id_Chat`))
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `MasterTheWeb`.`Messages`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `MasterTheWeb`.`Messages` (
-  `Id_message` INT NOT NULL AUTO_INCREMENT,
-  `Date_du_message` DATE NOT NULL,
-  `Utilisateur_id_Utilisateur` INT NULL,
-  `Contenu` VARCHAR(255) NOT NULL,
-  `Chat_id_Chat` INT NOT NULL,
-  PRIMARY KEY (`Id_message`),
-  INDEX `fk_Messages_Utilisateur1_idx` (`Utilisateur_id_Utilisateur` ASC) ,
-  INDEX `fk_Messages_Chat1_idx` (`Chat_id_Chat` ASC) ,
-  CONSTRAINT `fk_Messages_Utilisateur1`
-    FOREIGN KEY (`Utilisateur_id_Utilisateur`)
-    REFERENCES `MasterTheWeb`.`Utilisateur` (`id_Utilisateur`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_Messages_Chat1`
-    FOREIGN KEY (`Chat_id_Chat`)
-    REFERENCES `MasterTheWeb`.`Chat` (`id_Chat`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
 
 
 -- -----------------------------------------------------
 -- Table `MasterTheWeb`.`Journal`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `MasterTheWeb`.`Journal` (
-  `id_Journal` INT NOT NULL AUTO_INCREMENT,
+  `id_Journal` INT(11) NOT NULL AUTO_INCREMENT,
   `Titre` VARCHAR(100) NOT NULL,
   `date_création` DATE NOT NULL,
-  `Contenu` TEXT(2500) NOT NULL,
-  `Catégorie` VARCHAR(45) NOT NULL,
-  `Utilisateur_id_Utilisateur` INT NOT NULL,
-  PRIMARY KEY (`id_Journal`),
-  INDEX `fk_Journal_Utilisateur1_idx` (`Utilisateur_id_Utilisateur` ASC) ,
-  CONSTRAINT `fk_Journal_Utilisateur1`
-    FOREIGN KEY (`Utilisateur_id_Utilisateur`)
-    REFERENCES `MasterTheWeb`.`Utilisateur` (`id_Utilisateur`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+  `Contenu` TEXT NOT NULL,
+  PRIMARY KEY (`id_Journal`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
 
 
 -- -----------------------------------------------------
--- Table `MasterTheWeb`.`Commentaires`
+-- Table `MasterTheWeb`.`Utilisateur`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `MasterTheWeb`.`Commentaires` (
-  `id_Commentaires` INT NOT NULL AUTO_INCREMENT,
-  `Contenu` VARCHAR(255) NOT NULL,
-  `Utilisateur_id_Utilisateur` INT NULL,
-  `Journal_id_Journal` INT NOT NULL,
-  PRIMARY KEY (`id_Commentaires`),
-  INDEX `fk_Commentaires_Utilisateur1_idx` (`Utilisateur_id_Utilisateur` ASC) ,
-  INDEX `fk_Commentaires_Journal1_idx` (`Journal_id_Journal` ASC) ,
-  CONSTRAINT `fk_Commentaires_Utilisateur1`
-    FOREIGN KEY (`Utilisateur_id_Utilisateur`)
-    REFERENCES `MasterTheWeb`.`Utilisateur` (`id_Utilisateur`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_Commentaires_Journal1`
-    FOREIGN KEY (`Journal_id_Journal`)
-    REFERENCES `MasterTheWeb`.`Journal` (`id_Journal`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+CREATE TABLE IF NOT EXISTS `MasterTheWeb`.`Utilisateur` (
+  `id_Utilisateur` INT(11) NOT NULL AUTO_INCREMENT,
+  `Pseudo` VARCHAR(30) NOT NULL,
+  `Droits` VARCHAR(5) NOT NULL,
+  `e-mail` VARCHAR(45) NOT NULL,
+  `Mot_de_passe` VARCHAR(45) NOT NULL,
+  `date_de_naissance` DATE NOT NULL,
+  PRIMARY KEY (`id_Utilisateur`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
 
 
 -- -----------------------------------------------------
 -- Table `MasterTheWeb`.`Topic`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `MasterTheWeb`.`Topic` (
-  `id_Topic` INT NOT NULL AUTO_INCREMENT,
+  `id_Topic` INT(11) NOT NULL AUTO_INCREMENT,
   `Date_création` DATE NOT NULL,
-  `Contenu` TEXT(2500) NOT NULL,
-  `Utilisateur_id_Utilisateur` INT NULL,
-  `Commentaires_id_Commentaires` INT NOT NULL,
+  `Contenu` TEXT NOT NULL,
+  `Utilisateur_id_Utilisateur` INT(11) NOT NULL,
   PRIMARY KEY (`id_Topic`),
   INDEX `fk_Topic_Utilisateur1_idx` (`Utilisateur_id_Utilisateur` ASC) ,
-  INDEX `fk_Topic_Commentaires1_idx` (`Commentaires_id_Commentaires` ASC) ,
   CONSTRAINT `fk_Topic_Utilisateur1`
     FOREIGN KEY (`Utilisateur_id_Utilisateur`)
     REFERENCES `MasterTheWeb`.`Utilisateur` (`id_Utilisateur`)
     ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+
+-- -----------------------------------------------------
+-- Table `MasterTheWeb`.`Commentaires`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `MasterTheWeb`.`Commentaires` (
+  `id_Commentaires` INT(11) NOT NULL AUTO_INCREMENT,
+  `Contenu` VARCHAR(255) NOT NULL,
+  `Pseudo` VARCHAR(20) NOT NULL,
+  `Date` DATETIME NOT NULL,
+  `Utilisateur_id_Utilisateur` INT(11) NULL,
+  `Journal_id_Journal` INT(11) NULL,
+  `Topic_id_Topic` INT(11) NULL,
+  PRIMARY KEY (`id_Commentaires`),
+  INDEX `fk_Commentaires_Utilisateur1_idx` (`Utilisateur_id_Utilisateur` ASC) ,
+  INDEX `fk_Commentaires_Journal1_idx` (`Journal_id_Journal` ASC) ,
+  INDEX `fk_Commentaires_Topic1_idx` (`Topic_id_Topic` ASC) ,
+  CONSTRAINT `fk_Commentaires_Journal1`
+    FOREIGN KEY (`Journal_id_Journal`)
+    REFERENCES `MasterTheWeb`.`Journal` (`id_Journal`)
+    ON DELETE NO ACTION
     ON UPDATE NO ACTION,
-  CONSTRAINT `fk_Topic_Commentaires1`
-    FOREIGN KEY (`Commentaires_id_Commentaires`)
-    REFERENCES `MasterTheWeb`.`Commentaires` (`id_Commentaires`)
+  CONSTRAINT `fk_Commentaires_Utilisateur1`
+    FOREIGN KEY (`Utilisateur_id_Utilisateur`)
+    REFERENCES `MasterTheWeb`.`Utilisateur` (`id_Utilisateur`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_Commentaires_Topic1`
+    FOREIGN KEY (`Topic_id_Topic`)
+    REFERENCES `MasterTheWeb`.`Topic` (`id_Topic`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
 
 
 -- -----------------------------------------------------
--- Table `MasterTheWeb`.`Token`
+-- Table `MasterTheWeb`.`LearnToWin_Cours`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `MasterTheWeb`.`Token` (
-  `id_Token` INT NOT NULL AUTO_INCREMENT,
-  `Tx_Id` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`id_Token`))
-ENGINE = InnoDB;
+CREATE TABLE IF NOT EXISTS `MasterTheWeb`.`LearnToWin_Cours` (
+  `idLearnToWin_Cours` INT(11) NOT NULL AUTO_INCREMENT,
+  `Contenu` TEXT NOT NULL,
+  `Utilisateur_id_Utilisateur` INT(11) NOT NULL,
+  PRIMARY KEY (`idLearnToWin_Cours`),
+  INDEX `fk_LearnToWin_Cours_Utilisateur1_idx` (`Utilisateur_id_Utilisateur` ASC) ,
+  CONSTRAINT `fk_LearnToWin_Cours_Utilisateur1`
+    FOREIGN KEY (`Utilisateur_id_Utilisateur`)
+    REFERENCES `MasterTheWeb`.`Utilisateur` (`id_Utilisateur`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+
+-- -----------------------------------------------------
+-- Table `MasterTheWeb`.`Messages`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `MasterTheWeb`.`Messages` (
+  `Id_message` INT(11) NOT NULL AUTO_INCREMENT,
+  `Date_du_message` DATE NOT NULL,
+  `Utilisateur_id_Utilisateur` INT(11) NOT NULL,
+  `Contenu` VARCHAR(255) NOT NULL,
+  `Chat_id_Chat` INT(11) NOT NULL,
+  PRIMARY KEY (`Id_message`),
+  INDEX `fk_Messages_Utilisateur1_idx` (`Utilisateur_id_Utilisateur` ASC) ,
+  INDEX `fk_Messages_Chat1_idx` (`Chat_id_Chat` ASC) ,
+  CONSTRAINT `fk_Messages_Chat1`
+    FOREIGN KEY (`Chat_id_Chat`)
+    REFERENCES `MasterTheWeb`.`Chat` (`id_Chat`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_Messages_Utilisateur1`
+    FOREIGN KEY (`Utilisateur_id_Utilisateur`)
+    REFERENCES `MasterTheWeb`.`Utilisateur` (`id_Utilisateur`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
 
 
 -- -----------------------------------------------------
 -- Table `MasterTheWeb`.`Note`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `MasterTheWeb`.`Note` (
-  `id_Note` INT NOT NULL AUTO_INCREMENT,
-  `Valeur` INT NOT NULL,
+  `id_Note` INT(11) NOT NULL AUTO_INCREMENT,
+  `Valeur` INT(11) NOT NULL,
   `Date` DATE NOT NULL,
-  `Utilisateur_id_Utilisateur` INT NULL,
-  `Journal_id_Journal` INT NULL,
+  `Utilisateur_id_Utilisateur` INT(11) NOT NULL,
+  `Journal_id_Journal` INT(11) NOT NULL,
   PRIMARY KEY (`id_Note`),
   INDEX `fk_Note_Utilisateur1_idx` (`Utilisateur_id_Utilisateur` ASC) ,
   INDEX `fk_Note_Journal1_idx` (`Journal_id_Journal` ASC) ,
-  CONSTRAINT `fk_Note_Utilisateur1`
-    FOREIGN KEY (`Utilisateur_id_Utilisateur`)
-    REFERENCES `MasterTheWeb`.`Utilisateur` (`id_Utilisateur`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
   CONSTRAINT `fk_Note_Journal1`
     FOREIGN KEY (`Journal_id_Journal`)
     REFERENCES `MasterTheWeb`.`Journal` (`id_Journal`)
     ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `MasterTheWeb`.`Utilisateur_has_Token`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `MasterTheWeb`.`Utilisateur_has_Token` (
-  `Utilisateur_id_Utilisateur` INT NOT NULL AUTO_INCREMENT,
-  `Token_id_Token` INT NOT NULL,
-  `nombre` INT NOT NULL,
-  INDEX `fk_Utilisateur_has_Token_Token1_idx` (`Token_id_Token` ASC) ,
-  INDEX `fk_Utilisateur_has_Token_Utilisateur1_idx` (`Utilisateur_id_Utilisateur` ASC) ,
-  PRIMARY KEY (`Utilisateur_id_Utilisateur`, `Token_id_Token`),
-  CONSTRAINT `fk_Utilisateur_has_Token_Utilisateur1`
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_Note_Utilisateur1`
     FOREIGN KEY (`Utilisateur_id_Utilisateur`)
     REFERENCES `MasterTheWeb`.`Utilisateur` (`id_Utilisateur`)
     ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_Utilisateur_has_Token_Token1`
-    FOREIGN KEY (`Token_id_Token`)
-    REFERENCES `MasterTheWeb`.`Token` (`id_Token`)
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+
+-- -----------------------------------------------------
+-- Table `MasterTheWeb`.`Quizz`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `MasterTheWeb`.`Quizz` (
+  `idQuizz` INT(11) NOT NULL AUTO_INCREMENT,
+  `Questions` TEXT NOT NULL,
+  `LearnToWin_Cours_idLearnToWin_Cours` INT(11) NOT NULL,
+  `Reponse` INT(11) NOT NULL,
+  PRIMARY KEY (`idQuizz`),
+  INDEX `fk_Quizz_LearnToWin_Cours1_idx` (`LearnToWin_Cours_idLearnToWin_Cours` ASC) ,
+  CONSTRAINT `fk_Quizz_LearnToWin_Cours1`
+    FOREIGN KEY (`LearnToWin_Cours_idLearnToWin_Cours`)
+    REFERENCES `MasterTheWeb`.`LearnToWin_Cours` (`idLearnToWin_Cours`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+
+-- -----------------------------------------------------
+-- Table `MasterTheWeb`.`Token`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `MasterTheWeb`.`Token` (
+  `id_Token` INT(11) NOT NULL AUTO_INCREMENT,
+  `Tx_Id` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`id_Token`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
 
 
 -- -----------------------------------------------------
 -- Table `MasterTheWeb`.`Utilisateur_chat_Utilisateur`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `MasterTheWeb`.`Utilisateur_chat_Utilisateur` (
-  `Utilisateur_id_Utilisateur` INT NOT NULL AUTO_INCREMENT,
-  `Utilisateur_id_Utilisateur1` INT NOT NULL,
-  `contenu` TEXT(2500) NOT NULL,
+  `Utilisateur_id_Utilisateur` INT(11) NOT NULL AUTO_INCREMENT,
+  `Utilisateur_id_Utilisateur1` INT(11) NOT NULL,
+  `contenu` TEXT NOT NULL,
   `date` DATE NOT NULL,
+  PRIMARY KEY (`Utilisateur_id_Utilisateur`, `Utilisateur_id_Utilisateur1`),
   INDEX `fk_Utilisateur_has_Utilisateur_Utilisateur2_idx` (`Utilisateur_id_Utilisateur1` ASC) ,
   INDEX `fk_Utilisateur_has_Utilisateur_Utilisateur1_idx` (`Utilisateur_id_Utilisateur` ASC) ,
-  PRIMARY KEY (`Utilisateur_id_Utilisateur`, `Utilisateur_id_Utilisateur1`),
   CONSTRAINT `fk_Utilisateur_has_Utilisateur_Utilisateur1`
     FOREIGN KEY (`Utilisateur_id_Utilisateur`)
     REFERENCES `MasterTheWeb`.`Utilisateur` (`id_Utilisateur`)
@@ -211,42 +231,32 @@ CREATE TABLE IF NOT EXISTS `MasterTheWeb`.`Utilisateur_chat_Utilisateur` (
     REFERENCES `MasterTheWeb`.`Utilisateur` (`id_Utilisateur`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
 
 
 -- -----------------------------------------------------
--- Table `MasterTheWeb`.`LearnToWin_Cours`
+-- Table `MasterTheWeb`.`Utilisateur_has_Token`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `MasterTheWeb`.`LearnToWin_Cours` (
-  `idLearnToWin_Cours` INT NOT NULL AUTO_INCREMENT,
-  `Contenu` TEXT(2500) NOT NULL,
-  `Utilisateur_id_Utilisateur` INT NOT NULL,
-  PRIMARY KEY (`idLearnToWin_Cours`),
-  INDEX `fk_LearnToWin_Cours_Utilisateur1_idx` (`Utilisateur_id_Utilisateur` ASC) ,
-  CONSTRAINT `fk_LearnToWin_Cours_Utilisateur1`
+CREATE TABLE IF NOT EXISTS `MasterTheWeb`.`Utilisateur_has_Token` (
+  `Utilisateur_id_Utilisateur` INT(11) NOT NULL,
+  `Token_id_Token` INT(11) NOT NULL,
+  `nombre` INT(11) NOT NULL,
+  PRIMARY KEY (`Utilisateur_id_Utilisateur`, `Token_id_Token`),
+  INDEX `fk_Utilisateur_has_Token_Token1_idx` (`Token_id_Token` ASC) ,
+  INDEX `fk_Utilisateur_has_Token_Utilisateur1_idx` (`Utilisateur_id_Utilisateur` ASC) ,
+  CONSTRAINT `fk_Utilisateur_has_Token_Token1`
+    FOREIGN KEY (`Token_id_Token`)
+    REFERENCES `MasterTheWeb`.`Token` (`id_Token`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_Utilisateur_has_Token_Utilisateur1`
     FOREIGN KEY (`Utilisateur_id_Utilisateur`)
     REFERENCES `MasterTheWeb`.`Utilisateur` (`id_Utilisateur`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `MasterTheWeb`.`Quizz`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `MasterTheWeb`.`Quizz` (
-  `idQuizz` INT NOT NULL AUTO_INCREMENT,
-  `Questions` TEXT(250) NOT NULL,
-  `LearnToWin_Cours_idLearnToWin_Cours` INT NOT NULL,
-  `Reponse` INT NOT NULL,
-  PRIMARY KEY (`idQuizz`),
-  INDEX `fk_Quizz_LearnToWin_Cours1_idx` (`LearnToWin_Cours_idLearnToWin_Cours` ASC) ,
-  CONSTRAINT `fk_Quizz_LearnToWin_Cours1`
-    FOREIGN KEY (`LearnToWin_Cours_idLearnToWin_Cours`)
-    REFERENCES `MasterTheWeb`.`LearnToWin_Cours` (`idLearnToWin_Cours`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
 
 
 SET SQL_MODE=@OLD_SQL_MODE;
---
+++ journal.php
@@ -0,0 +1,98 @@
+<?php
+if (!isset($_GET['id_Journal']) OR !is_numeric($_GET['id_Journal'])) 
+    header('Location: index.php');
+else
+{
+    extract($_GET);
+    $id_Journal = strip_tags($id_Journal);
+
+    require_once('LeSuPerisien/fonctions.php');
+    if (!empty($_POST))
+    {
+        extract($_POST);
+        $errors = array();
+
+        $author = strip_tags($author);
+        $comment = strip_tags($comment);
+
+        if(empty($author))
+            array_push($errors, 'Entrez un pseudo');
+        
+        if(empty($comment))
+            array_push($errors, 'Entrez un commentaire');
+
+        if (count($errors) == 0)
+        {
+            $comment =addComment($id_Journal, $author, $comment);
+
+            $success = 'Votre commentaire a été publié';
+
+            unset($author);
+            unset($comment);
+        }
+            
+    }
+
+    $journal = getJournal($id_Journal);
+    $comments = getComments($id_Journal);
+}
+
+?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?= $journal->Titre ?></title>
+</head>
+<body>
+    <header>
+        <?php include "./component/header.php"; ?>
+    </header>
+    <main class="container">
+        <div class="container py-4">
+            <div class="p-5 mb-4 bg-body-tertiary rounded-3">
+                <h1><?= $journal->Titre ?></h1>
+                <p><?= $journal->Contenu ?></p>
+                <hr />
+                <time> <?= $journal->date_création ?> </time>
+            </div>
+        </div>
+
+        <?php 
+        if(isset($success))
+            echo $success;
+
+        if(!empty($errors)):?>
+
+            <?php foreach($errors as $error): ?>
+            <p><?= $error ?></p>
+            <?php endforeach; ?>
+        <?php endif; ?>
+        <form action="journal.php?id_Journal=<?= $journal->id_Journal ?>" method ="post">
+            <p> <label for="author">Pseudo :</label><br/>
+            <input type="text" name="author" id="author" value="<?php if(isset($author)) echo $author ?>" /></p>
+            <p> <label for="comment"> Commentaire : </label> </br>
+            <textarea name ="comment" id="comment" cols="30" rows="4" > <?php if(isset($comment)) echo $comment ?></textarea></p>
+            <button type="submit">Envoyer</button>
+        </form>
+
+        <div class="container py-4">
+        <h2> Commentaire(s) : </h2>
+            <div class="p-5 mb-4 bg-body-tertiary rounded-3">
+                
+
+                <?php foreach($comments as $comment): ?>
+                    <h3><?= $comment-> Pseudo?></h3>
+                    <time><?= $comment->Date ?> </time>
+                    <p> <?= $comment-> Contenu ?> </p>
+                <?php endforeach; ?>
+            </div>
+        </div>
+    </main>
+    <footer>
+        <?php include "./component/footer.php"; ?>
+    </footer>
+</body>
+</html>
