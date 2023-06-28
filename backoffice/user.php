2ff2b293f270cbed8f533cd051e272b3f2dd2b26
rhabel5 <rhabel@myges.fr>
2023-06-28 23:34:04 +0200

    fonction ban user drag n drop ban semi fonctionnel hide password user

--- backoffice/user.php
+++ backoffice/user.php
@@ -8,6 +8,8 @@
     <title>SuperBackOffice</title>
     <link rel="stylesheet" href="./style.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <script src="./dragndrop.js"></script>
+    <script src="./password.js"></script>
     <?php
     require_once "../BDD/config.php";
     session_start();
@@ -17,7 +19,7 @@
        exit;
     }
 
-      if ($_SESSION['droits'] !== 'admin') {
+    if ($_SESSION['droits'] !== 'admin') {
         header("Location: ../profil.php?error=notadmin");
         exit();
     }
@@ -28,7 +30,7 @@
         <img src="../img/badge/staffbadge.png" alt="logo SuperCoin" id="logo"/>
         <div class="user-info">
             <img src="../img/picture/pp.png" alt="Photo de profil" class="profile-picture" id="pp"/>
-            <div class="username mt-2 col-md-3 ">
+            <div class="username mt-2 col-md-3">
             <?php 
             if (!isset($_SESSION['Pseudo'])) {
                 $_SESSION['Pseudo'] = "root";
@@ -56,7 +58,7 @@
         <div class="main-content">
             <h3>Utilisateurs</h3>
             <?php
-                if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["addUser"])) {
+            if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["addUser"])) {
                 $pseudo = $_POST["pseudo"];
                 $userpassword = $_POST["mot_de_passe"];
                 $droits = $_POST["droits"];
@@ -68,94 +70,146 @@
                 } else {
                     echo "Erreur lors de l'ajout de l'utilisateur: " . $bdd->error;
                 }
-                }
+            }
             ?>
             <?php
-                // Requête SQL pour récupérer les informations de l'utilisateur
-                $sql = "SELECT * FROM `utilisateur` ORDER BY `id_Utilisateur` ASC";
-                $result = $bdd->query($sql);
+            // Requête SQL pour récupérer les informations de l'utilisateur
+            $sql = "SELECT * FROM `utilisateur` ORDER BY `id_Utilisateur` ASC";
+            $result = $bdd->query($sql);
 
-                if (isset($_GET['delete'])) {
-                    $delete = $_GET['delete'];
-                    // Vérifier si l'utilisateur existe avant de supprimer
-                    $checkUserSql = "SELECT * FROM `utilisateur` WHERE `id_Utilisateur` = ?";
-                    $stmt = $bdd->prepare($checkUserSql);
-                    $stmt->bind_param("i", $delete);
-                    $stmt->execute();
-                    $stmt->store_result();
-                    if ($stmt->num_rows > 0) {
-                        // Afficher une confirmation de suppression
-                        echo "<script>
-                                var result = confirm('Êtes-vous sûr de vouloir supprimer cet utilisateur?');
-                                if (result) {
-                                    window.location.href = '?delete_confirmed=' + $delete;
-                                }
-                            </script>";
-                    } else {
-                        echo "Utilisateur introuvable.";
-                    }
-                    $stmt->close();
-                }
-            
-                if (isset($_GET['delete_confirmed'])) {
-                    $deleteConfirmed = $_GET['delete_confirmed'];
-                    $sql = "DELETE FROM `utilisateur` WHERE `id_Utilisateur` = ?";
-                    $stmt = $bdd->prepare($sql);
-                    $stmt->bind_param("i", $deleteConfirmed);
-                    $stmt->execute();
-                    $stmt->close();
-                    // Rediriger vers la page principale après suppression
-                    header("Location: ./user.php");
-                    exit();
-                }
-
-                if (isset($_GET['isban'])) {
-                    $isban = $_GET['isban'];
-                    $sql = "UPDATE `utilisateur` SET `isban` = 'true' WHERE `id_Utilisateur` = ?";
-                    $stmt = $bdd->prepare($sql);
-                    $stmt->bind_param("i", $isban);
-                    $stmt->execute();
-                    $stmt->close();
-                }
-
-                if ($result->rowCount() > 0) {
-                    // Récupération des données de l'utilisateur
-                    echo '<p>Gestion des utilisateurs :</p>';
-                    echo '<table class="table table-condensed table-striped">';
-                    echo '<tr> <th> Id </th> <th> Pseudo </th> <th> Email </th> <th> Date de naissance </th> <th> Droits </th> <th> Banni? </th> <th> Supprimer </th> <th> Bannir </th> </tr>';
-                    // Affichage des informations de l'utilisateur
-                    while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
-                        echo '<tr>';
-                        echo '<td>' .$row["id_Utilisateur"]. '</td>';
-                        echo "<td>" .$row["Pseudo"]. "</td>";
-                        echo "<td>" .$row["e-mail"]. "</td>";
-                        echo "<td> " . $row["date_de_naissance"] . "</td>";
-                        echo "<td>" .$row["Droits"]. "</td>";
-                        echo "<td>" .$row["isban"]. "</td>";
-                        echo "<td> <a class='badge badge-danger' href='?delete=" . $row["id_Utilisateur"] . "'>Supprimer</a> </td>";
-                        echo "<td> <a class='badge badge-warning' href='?isban=" . $row["id_Utilisateur"] . "'>Bannir</a> </td>";
-                        echo '</tr>';
-                    }
+            if (isset($_GET['delete'])) {
+                $delete = $_GET['delete'];
+                // Vérifier si l'utilisateur existe avant de supprimer
+                $checkUserSql = "SELECT * FROM `utilisateur` WHERE `id_Utilisateur` = :id";
+                $stmt = $bdd->prepare($checkUserSql);
+                $stmt->bindValue(":id", $delete, PDO::PARAM_INT);
+                $stmt->execute();
+                $rowCount = $stmt->rowCount();                
+                if ($rowCount > 0) {
+                    // Afficher une confirmation de suppression
+                    echo "<script>
+                            var result = confirm('Êtes-vous sûr de vouloir supprimer cet utilisateur?');
+                            if (result) {
+                                window.location.href = '?delete_confirmed=' + $delete;
+                            }
+                        </script>";
                 } else {
-                    echo "Aucun utilisateur trouvé.";
+                    echo "Utilisateur introuvable.";
                 }
+                $stmt->close();
+            }
 
-                $bdd = null;
+            if (isset($_GET['delete_confirmed'])) {
+                $deleteConfirmed = $_GET['delete_confirmed'];
+                $sql = "DELETE FROM `utilisateur` WHERE `id_Utilisateur` = :id";
+                $stmt = $bdd->prepare($sql);
+                $stmt->bindValue(":id", $deleteConfirmed, PDO::PARAM_INT);
+                $stmt->execute();
+                $stmt->closeCursor();
+                header("Location: ./user.php");
+                exit();
+            }
+            
+            if (isset($_POST['banUser']) && isset($_POST['banUserId'])) {
+                $banUserId = $_POST['banUserId'];
+                $sql = "UPDATE utilisateur SET isban = 1 WHERE id_Utilisateur = :id";
+                $stmt = $bdd->prepare($sql);
+                $stmt->bindParam(':id', $banUserId);
+                $stmt->execute();
+            }
+
+            if (isset($_POST['unbanUser']) && isset($_POST['unbanUserId'])) {
+                $unbanUserId = $_POST['unbanUserId'];
+                $sql = "UPDATE utilisateur SET isban = 0 WHERE id_Utilisateur = :id";
+                $stmt = $bdd->prepare($sql);
+                $stmt->bindParam(':id', $unbanUserId);
+                $stmt->execute();
+            }            
+
+            if ($result->rowCount() > 0) {
+                echo '<p>Gestion des utilisateurs :</p>';
+                echo '<table class="table table-condensed table-striped">';
+                echo '<tr> <th> Id </th> <th> Pseudo </th> <th> Email </th> <th> Mot de passe </th> <th> Date de naissance </th> <th> Droits </th> <th> Banni? </th> <th> Supprimer </th> <th> Bannir </th> <th> Voir </th> </tr>';
+                while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
+                    echo '<tr draggable="true">';
+                    echo '<td>' . $row["id_Utilisateur"] . '</td>';
+                    echo "<td>" . $row["Pseudo"] . "</td>";
+                    echo "<td>" . $row["e-mail"] . "</td>";
+                    echo '<td><span class="password" data-password="' . $row["Mot_de_passe"] . '">********</span></td>';
+                    echo "<td> " . $row["date_de_naissance"] . "</td>";
+                    echo "<td>" . $row["Droits"] . "</td>";
+                    echo "<td>" . $row["isban"] . "</td>";
+                    echo "<td> <a class='badge badge-danger' href='?delete=" . $row["id_Utilisateur"] . "'>Supprimer</a> </td>";
+                    echo "<td>
+                            <form action='' method='POST'>
+                                <input type='hidden' name='banUserId' value='" . $row["id_Utilisateur"] . "'>
+                                <button type='submit' name='banUser' class='badge badge-warning'>Bannir</button>
+                            </form>
+                        </td>";            
+                    echo "<td> <button class='btn btn-sm btn-outline-secondary show-password' onclick='togglePassword(this)'>Voir</button> </td>";
+                    echo '</tr>';
+                }
+                echo '</table>';
+            } else {
+                echo "Aucun utilisateur trouvé.";
+            }
+
+            $sqlBanned = "SELECT * FROM `utilisateur` WHERE `isban` = 1 ORDER BY `id_Utilisateur` ASC";
+            $resultBanned = $bdd->query($sqlBanned);
+
+            if ($resultBanned->rowCount() > 0) {
+                echo '<h3>Utilisateurs bannis</h3>';
+                echo '<table class="table table-condensed table-striped">';
+                echo '<tr> <th> Id </th> <th> Pseudo </th> <th> Email </th> <th> Date de naissance </th> <th> Droits </th> <th> Débannir </th> </tr>';
+                while ($rowBanned = $resultBanned->fetch(PDO::FETCH_ASSOC)) {
+                    echo '<tr class="banned" draggable="true">';
+                    echo '<td>' . $rowBanned["id_Utilisateur"] . '</td>';
+                    echo "<td>" . $rowBanned["Pseudo"] . "</td>";
+                    echo "<td>" . $rowBanned["e-mail"] . "</td>";
+                    echo "<td> " . $rowBanned["date_de_naissance"] . "</td>";
+                    echo "<td>" . $rowBanned["Droits"] . "</td>";
+                    echo "<td>
+                            <form action='' method='POST'>
+                                <input type='hidden' name='unbanUserId' value='" . $rowBanned["id_Utilisateur"] . "'>
+                                <button type='submit' name='unbanUser' class='badge badge-success'>Débannir</button>
+                            </form>
+                        </td>";            
+                    echo '</tr>';
+                }
+                echo '</table>';
+            } else {
+                echo "Aucun utilisateur banni trouvé.";
+            }
+
+            $bdd = null;
             ?>
             <form method="POST">
-                <label for="pseudo">Pseudo:</label> <input type="text" name="pseudo" required>
-                <label for="mot_de_passe">Mot de passe:</label> <input type="password" name="mot_de_passe" required>
-                <label for="email">Email:</label> <input type="email" name="email" required>
-                <label for="date_de_naissance">Date de naissance:</label> <input type="date" name="date_de_naissance" required>
-                <label for="droits">Droits:</label>
-                <select name="droits" id="droits">
-                <option value="admin">admin</option>
-                <option value="user">user</option>
-                </select>
-                <button type="submit" name="addUser">Ajouter</button>
-                <button type="submit" name="exp">Modifier</button>
+                <div class="form-group">
+                    <label for="pseudo">Pseudo:</label>
+                    <input type="text" class="form-control" name="pseudo" required>
+                </div>
+                <div class="form-group">
+                    <label for="mot_de_passe">Mot de passe:</label>
+                    <input type="password" class="form-control" name="mot_de_passe" required>
+                </div>
+                <div class="form-group">
+                    <label for="email">Email:</label>
+                    <input type="email" class="form-control" name="email" required>
+                </div>
+                <div class="form-group">
+                    <label for="date_de_naissance">Date de naissance:</label>
+                    <input type="date" class="form-control" name="date_de_naissance" required>
+                </div>
+                <div class="form-group">
+                    <label for="droits">Droits:</label>
+                    <select class="form-control" name="droits" id="droits">
+                        <option value="admin">admin</option>
+                        <option value="user">user</option>
+                    </select>
+                </div>
+                <button type="submit" class="btn btn-primary" name="addUser">Ajouter</button>
             </form>
         </div>
     </div>
-  </body>
+</body>
 </html>
