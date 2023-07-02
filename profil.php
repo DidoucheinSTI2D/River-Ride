8888056ea2f0f13a8c98d007718b2469f8a7b209
rhabel5 <rhabel@myges.fr>
2023-07-02 22:22:35 +0200

    gestion erreurs pas fini mode sombre pas fini

--- profil.php
+++ profil.php
@@ -5,7 +5,6 @@
         exit;
     }
     $pseudo = $_SESSION['Pseudo'];
-
 ?>
 
 <!DOCTYPE html>
@@ -16,6 +15,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>LeSuperCoin - Profil</title>
+    <link rel="stylesheet" href="./style.css">
     <link rel="stylesheet" href="./component/avatar.css">
 </head>
 
@@ -31,11 +31,11 @@
         <div class="row justify-content-center p-3 mb-2 bg-light text-dark rounded mx-auto">
             <div class="tab-content">
                 <p style="color: red;">
-                <?php
-                if(isset($_GET['error']) && $_GET['error'] ==  "notadmin"){
-                    echo "Vous n'avez pas les permissions requises pour accéder à cette page 😡";
-                }
-                ?>
+                    <?php
+                    if(isset($_GET['error']) && $_GET['error'] ==  "notadmin"){
+                        echo "Vous n'avez pas les permissions requises pour accéder à cette page 😡";
+                    }
+                    ?>
                 </p>
                 <div class="tab-pane fade show active">
                     <div class="text-center mb-3">
@@ -44,6 +44,7 @@
                         <button class="btn btn-primary btn-block"> Modifier les informations </button>
                         <a href="avatar.php"><button class="btn btn-primary btn-block"> Modifier l'avatar </button></a>
                         <a href="disconnect.php"><button class="btn btn-danger btn-block">Se déconnecter</button></a>
+                        <div class="changeTheme"><button class="btn btn-dark btn-block">Basculer le mode sombre</button></div>
                     </div>
                 </div>
             </div>
@@ -51,12 +52,24 @@
     </div>
 </main>
 
-
 <footer>
     <?php include "./component/footer.php"; ?>
 </footer>
 
+<script>
+    const switchThemeBtn = document.querySelector('.changeTheme button');
+    const body = document.body;
+    let isDarkMode = false;
 
-
+    switchThemeBtn.addEventListener('click', () => {
+        isDarkMode = !isDarkMode;
+        if (isDarkMode) {
+            body.classList.add('dark');
+        } else {
+            body.classList.remove('dark');
+        }
+    });
+</script>
 
 </body>
+</html>
